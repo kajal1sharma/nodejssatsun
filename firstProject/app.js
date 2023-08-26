dd368fadcbfde43c31f38da120ef3ccd0f6426d0
kajal <kajal@gmail.com>
2023-08-26 12:08:21 +0530

    ..

--- firstProject/app.js
+++ firstProject/app.js
@@ -3,6 +3,7 @@ const http = require("http");
 const server = http.createServer((req,res)=>{
 
     res.write("<h1>this is app.js file</h1>")
+    res.write("Dont send node modules")
     res.write("<h1>the request has been processed</h1>")
     res.write("<h1>the nodemon package has been installed</h1>")
     res.end("<h2>the is end Line</h2>");
